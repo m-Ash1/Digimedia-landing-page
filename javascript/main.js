b8d3e1534f5d37c26541a8b6eae5db5ddb617b1c
Mahmoud Ashraf <mahmoudashraf2702@gmail.com>
2021-11-06 01:57:25 +0200

    add 2 sections

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,10 @@
-/*
-  String Challenge
-  All Solutions Must Be In One Chain
-  You Can Use Concatenate
-*/
+let att = document.querySelector(".container .desc");
+console.log(att.textContent);
+if (!att.hasAttribute("alt")) {
+  att.setAttribute("alt", "ZEBY")
+  console.log(`Yes`);
 
-let a = "Elzero Web School";
-
-// Include This Method In Your Solution [slice, charAt]
-console.log(a.charAt(2).toUpperCase()+a.slice(3,6)); // Zero
-
-// 8 H
-console.log(a.charAt(13).toUpperCase().repeat(7)); // HHHHHHHH
-
-// Return Array
-console.log(a.split(" ",1)); // ["Elzero"]
-
-// Use Only "substr" Method + Template Literals In Your Solution
-console.log(`${a.split(" ")[0]} ${a.split(" ")[2]}`); // Elzero School
-
-// Solution Must Be Dynamic Because String May Changes
-console.log(`${a.slice(0,2).toLowerCase()}${a.slice(2).toUpperCase()}`); // eLZERO WEB SCHOOl
+}
+else{
+  console.log(`Nah Nah`)
+}
